7c30f1d73ddcf15bf932299655cf08b42dbf8c4b
Nathan Chabot <nathanchabot163@gmail.com>
2025-01-29 10:44:54 -0500

    Debug

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const btPlus = document.querySelector('.js-plus');
 
-const conteneurTransport = document.querySelector('.conteneur-transports');
+const conteneurTransport = document.querySelector('.conteneur-transport');
 
 btPlus.addEventListener('click', fouvreferme);
 
 
 function fouvreferme(){
+    /*console.log('coucou')*/
     conteneurTransport.classList.toggle('conteneur-transports-invisible');
 };
 
